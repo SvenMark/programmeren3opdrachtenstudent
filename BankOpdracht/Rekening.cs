04dca1ea4c2558dbbd85546dcc0c58598dcc79c5
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2017-02-10 11:39:20 +0100

    Begin met bank opdracht

--- BankOpdracht/Rekening.cs
+++ BankOpdracht/Rekening.cs
@@ -8,6 +8,36 @@ namespace BankOpdracht
 {
     public class Rekening
     {
-       
+        private double rekeningSaldo;
+        private double rekeningRood;
+        private int rekeningNummer;
+        private string rekeningNaam;
+        private string rekeningAdres;
+
+        public Rekening()
+        {
+            
+        }
+
+        public void GeldOpnemen()
+        {
+            
+        }
+
+        public void GeldStorten()
+        {
+            
+        }
+
+        public void GeldOvermaken()
+        {
+            
+        }
+
+        public string ToString()
+        {
+            return String.Format("Naam: {0} | RekeningNr: {1} | Saldo: {2}", rekeningNaam, rekeningNummer, rekeningSaldo);
+        }
+
     }
 }
