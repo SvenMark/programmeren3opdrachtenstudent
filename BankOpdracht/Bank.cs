21f57b6fd0677813861d893b28c427d79f3032aa
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2017-02-10 11:39:20 +0100

    Begin met bank opdracht

--- BankOpdracht/Bank.cs
+++ BankOpdracht/Bank.cs
@@ -9,5 +9,28 @@ namespace BankOpdracht
 {
     public class Bank
     {
+        private string bankNaam;
+        private List<Rekening> Rekeningen;
+        private int index = 0;
+
+        public Bank(string bankNaam)
+        {
+            this.bankNaam = bankNaam;
+        }
+
+        public void OpenRekening()
+        {
+            Rekening  = new Rekening();
+        }
+
+        public void VerwijderRekening()
+        {
+            
+        }
+
+        public void ZoekRekening()
+        {
+            
+        }
     }
 }
