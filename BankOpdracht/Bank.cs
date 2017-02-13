8b02648be2bb672c19b29cbe3e1e617ddd95861d
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2017-02-13 16:42:36 +0100

    bank helemaal af

--- BankOpdracht/Bank.cs
+++ BankOpdracht/Bank.cs
@@ -10,31 +10,84 @@ namespace BankOpdracht
     public class Bank
     {
         private string naam;
-        private int accountNumber;
-
-        public Bank(string naam)
-        {
-            this.naam = naam;
-        }
+        private List<Rekening> rekeningen = new List<Rekening>();
 
         public string Naam
         {
             get { return naam; }
         }
 
+        public Bank(string naam)
+        {
+            this.naam = naam;
+        }
+
         public int OpenRekening(string firstName, string lastName, string houseNumber, string postalCode)
         {
-            return accountNumber;
+            int idx = rekeningen.Count();
+            rekeningen.Add(new Rekening(idx, firstName, lastName, houseNumber, postalCode));
+            return idx;
         }
 
         public int OpenRekening(string firstName, string lastName, string houseNumber, string postalCode, int startSaldo)
         {
-            return accountNumber;
+            //klant mag niet een rekening openen met een negatief saldo
+            if (startSaldo < 0) return -1;
+            int idx = rekeningen.Count();
+            rekeningen.Add(new Rekening(idx, firstName, lastName, houseNumber, postalCode, startSaldo));
+            return idx;
         }
 
-        public int OpenRekening(string firstName, string lastName, string houseNumber, string postalCode, int startSaldo, int salary)
+        public int OpenRekening(string firstName, string lastName, string houseNumber, string postalCode, int startSaldo, int salaris)
         {
-            return accountNumber;
+            //klant mag niet een rekening openen met een negatief saldo
+            if (startSaldo < 0) return -1;
+            int idx = rekeningen.Count();
+            rekeningen.Add(new Rekening(idx, firstName, lastName, houseNumber, postalCode, startSaldo, salaris));
+            return idx;
+        }
+
+        public Rekening ZoekRekening(int rekeningNummer)
+        {
+            return rekeningen[rekeningNummer];
+
+        }
+
+        public Rekening VerwijderRekening(int rekeningNummer)
+        {
+            //rekeningen.Remove() kan niet want dan schuiven de rekeningnummers op en komen de indexes niet meer overeen.
+            Rekening rekening = rekeningen[rekeningNummer];
+            rekeningen[rekeningNummer] = null;
+            return rekening;
+        }
+
+        public decimal RenteUitkeren(decimal percentage)
+        {
+            decimal totaalUitgekeerd = 0;
+            foreach (Rekening r in rekeningen)
+            {
+                decimal rente = 0;
+                if (r.Saldo > 0)
+                {
+                    rente = r.Saldo * (percentage/100);
+                    totaalUitgekeerd += rente;
+                    r.GeldStorten(rente);
+                }
+            }
+            return totaalUitgekeerd;
+        }
+
+        public string ToString()
+        {
+            return "Naam van bank " + naam;
+
+            //Console.WriteLine("---------------------------");
+            //Console.WriteLine("Naam van bank: {0}", naam);
+            //Console.WriteLine("---------------------------");
+            //foreach (Rekening r in rekeningen)
+            //{
+            //    ToString()
+            //}
         }
     }
 }
--- BankOpdracht/Rekening.cs
+++ BankOpdracht/Rekening.cs
@@ -8,6 +8,70 @@ namespace BankOpdracht
 {
     public class Rekening
     {
-        
+        private int rekeningNummer;
+        private decimal saldo;
+        private decimal maxRood;
+        private string voornaam;
+        private string achternaam;
+        private string huisnr;
+        private string postcode;
+
+        public int Nr
+        {
+            get { return rekeningNummer; }
+        }
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+
+        public Rekening(int rekeningNummer, string voornaam, string achternaam, string huisnr, string postcode)
+        {
+            this.rekeningNummer = rekeningNummer;
+            this.voornaam = voornaam;
+            this.achternaam = achternaam;
+            this.huisnr = huisnr;
+            this.postcode = postcode;
+        }
+
+        public Rekening(int rekeningNummer, string voornaam, string achternaam, string huisnr, string postcode, decimal startSaldo, decimal maxRood = 0)
+        {
+            this.rekeningNummer = rekeningNummer;
+            this.voornaam = voornaam;
+            this.achternaam = achternaam;
+            this.huisnr = huisnr;
+            this.postcode = postcode;
+            saldo = startSaldo;
+            this.maxRood = maxRood/-2;
+        }
+
+        public bool GeldStorten(decimal bedrag)
+        {
+            if (bedrag <= 0) return false;
+            saldo += bedrag;
+            return true;
+        }
+
+        public bool GeldOpnemen(decimal bedrag)
+        {
+            if (bedrag <= 0) return false;
+            if ((saldo - bedrag) < maxRood) return false;
+            saldo -= bedrag;
+            return true;
+        }
+
+        public bool GeldOvermaken(Rekening tegenRekening, decimal bedrag)
+        {
+            if ((saldo - bedrag) < maxRood) return false;
+            saldo -= bedrag;
+            tegenRekening.GeldStorten(bedrag);
+            return true;
+        }
+
+        public string ToString()
+        {
+            return voornaam + " " + rekeningNummer + " " + saldo;
+        }
     }
 }
